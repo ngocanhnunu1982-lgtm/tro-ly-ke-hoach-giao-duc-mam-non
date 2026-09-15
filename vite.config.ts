e17b6d861d7a3067124b4045c5a6ecb2b0b94a91
ngocanhnunu1982-lgtm <ngocanh.nunu.1982@gmail.com>
2026-09-15 22:20:12 +0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 import { fileURLToPath, URL } from 'node:url';
 
 // https://vitejs.dev/config/
-export default defineConfig({
+export default defineConfig({base:'/tro-ly-ke-hoach-giao-duc-mam-non/',
   plugins: [react()],
   resolve: {
     alias: {
